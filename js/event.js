383c3628a9c1e4f383a6584b491c1b3bb54569de
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-20 02:14:17 +0600

    added evenlistener's code

--- js/event.js
+++ js/event.js
@@ -30,3 +30,33 @@ magentaColor.onclick = mmagenta;
 function mmagenta() {
     document.body.style.backgroundColor = "magenta"
 }
+
+const greenButton = document.getElementById('make-green-button');
+console.log(greenButton);
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = "green";
+}
+
+// handle by using add evenlistener
+const darkRed = document.getElementById('make-darkRed');
+darkRed.addEventListener('click', darkRedButton);
+function darkRedButton() {
+    document.body.style.backgroundColor = 'darkRed';
+}
+
+const greenyellow = document.getElementById('make-GreenYellow');
+greenyellow.addEventListener('click', green0);
+function green0() {
+    document.body.style.backgroundColor = 'GreenYellow';
+}
+
+// handle evenlistener 
+const oliveButton = document.getElementById('make-Olive-button');
+oliveButton.addEventListener('click', function olive8() {
+    document.body.style.backgroundColor = "Olive"
+})
+
+// direct shortcut 
+document.getElementById('make-NavajoWhite').addEventListener('click', function fd() {
+    document.body.style.backgroundColor = "NavajoWhite";
+})
